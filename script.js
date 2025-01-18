20d62d76d81650abbdc38ccd31cb5cfce4a895bb
Amir213902 <163657627+Amir213902@users.noreply.github.com>
2025-01-18 14:29:59 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const name_firstname = document.getElementById('name_firstname');
+const price = document.getElementById('price');
+const error_box = document.getElementById('error_box');
+const btn_do = document.getElementById('btn_do');
+
+function switchDisplayErrorBox () {
+    if(error_box.style.display == 'flex'){
+        error_box.style.display = 'none';
+    }else{
+        error_box.style.display = 'flex';
+    }
+}
+
+btn_do.addEventListener('click', () =>{
+    // switchDisplayErrorBox();
+    if(name_firstname.value == '' && price.value == '') {
+        switchDisplayErrorBox();
+    }else{
+        if(error_box.style.display == 'flex') {
+            switchDisplayErrorBox();
+        }
+        price_update = price.value + " ₽";
+        objectShop = {
+            "name": name_firstname.value,
+            "price": price_update
+        }
+        objectShopJSON = JSON.stringify(objectShop)
+        console.log(objectShop);
+        localStorage.setItem('obj', objectShopJSON);
+        window.location.href = "/nextStep/index.html";
+    }
+});
