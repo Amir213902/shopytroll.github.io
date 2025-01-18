abcc84ee3f9ca43e845e99e97bbc83636b7486f6
Amir213902 <163657627+Amir213902@users.noreply.github.com>
2025-01-18 15:43:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -27,6 +27,6 @@ btn_do.addEventListener('click', () =>{
         objectShopJSON = JSON.stringify(objectShop)
         console.log(objectShop);
         localStorage.setItem('obj', objectShopJSON);
-        window.location.href = "/nextStep/index.html";
+        window.location.href = "https://github.com/Amir213902/shopytroll.github.io/nextStep/";
     }
 });
